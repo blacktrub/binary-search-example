a1391b7fc3f30779e1367fed0557c5d2f094d0d1
blacktrub <lightlocdog@gmail.com>
2021-10-05 20:45:59 +0300

    add recursive implementation

--- bs.py
+++ bs.py
@@ -2,4 +2,15 @@ EMPTY_VALUE = -1
 
 
 def binary_search(rows: list, target: int) -> int:
-    return EMPTY_VALUE
+    if not rows:
+        return EMPTY_VALUE
+
+    middle = len(rows) // 2
+    middle_val = rows[middle]
+    if middle_val == target:
+        return middle
+
+    if middle_val > target:
+        return binary_search(rows[:middle], target)
+    else:
+        return binary_search(rows[middle + 1:], target) + middle + 1
--- test_bs.py
+++ test_bs.py
@@ -3,3 +3,23 @@ from bs import binary_search, EMPTY_VALUE
 
 def test_empty_binary_search():
     assert binary_search([], 1) == EMPTY_VALUE
+
+
+def test_two_elements():
+    assert binary_search([1, 2], 1) == 0
+
+
+def test_first_middle_element():
+    assert binary_search([1, 2, 3], 2) == 1
+
+
+def test_last_element():
+    assert binary_search([1, 2, 3], 3) == 2
+
+
+def test_first_element():
+    assert binary_search([1, 2, 3], 1) == 0
+
+
+def test_second_element_in_big_array():
+    assert binary_search([1, 2, 3, 4, 5, 6, 7], 3) == 2
